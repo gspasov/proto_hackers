0381856f1c7f630c868bcc0d75b25cbc47692270
Georgi Spasov <gvspasov@gmail.com>
2023-04-29 15:49:22 +0300

    fix: Add new line on the end of each message for `BudgetChat`

--- lib/proto_hackers/budget_chat/message_builder.ex
+++ lib/proto_hackers/budget_chat/message_builder.ex
@@ -1,22 +1,22 @@
 defmodule ProtoHackers.BudgetChat.MessageBuilder do
   @spec welcome() :: String.t()
-  def welcome, do: "Welcome to budget chat! What shall I call you?"
+  def welcome, do: "Welcome to budget chat! What shall I call you?\n"
 
   @spec join(username :: String.t()) :: String.t()
-  def join(username), do: "* #{username} has entered the room"
+  def join(username), do: "* #{username} has entered the room\n"
 
   @spec leave(username :: String.t()) :: String.t()
-  def leave(username), do: "* #{username} has left the room"
+  def leave(username), do: "* #{username} has left the room\n"
 
   @spec message(username :: String.t(), message :: String.t()) :: String.t()
-  def message(username, message), do: "[#{username}] #{message}"
+  def message(username, message), do: "[#{username}] #{message}\n"
 
   @spec participants([String.t()]) :: String.t()
   def participants(usernames)
 
-  def participants([]), do: "* The room is empty.."
+  def participants([]), do: "* The room is empty..\n"
 
   def participants(usernames) do
-    "* The room contains" <> Enum.join(usernames, ", ")
+    "* The room contains" <> Enum.join(usernames, ", ") <> "\n"
   end
 end

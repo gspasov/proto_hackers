48be7716c96714f2e19acef676e09990348ab69d
Georgi Spasov <gvspasov@gmail.com>
2023-04-29 14:08:50 +0300

    feat: Add `BudgetChat` to application and move ports in the 5000 range

--- lib/proto_hackers/application.ex
+++ lib/proto_hackers/application.ex
@@ -6,13 +6,15 @@ defmodule ProtoHackers.Application do
   alias ProtoHackers.SmokeTest
   alias ProtoHackers.PrimeTime
   alias ProtoHackers.MeansToAnEnd
+  alias ProtoHackers.BudgetChat
 
   @impl true
   def start(_type, _args) do
     children = [
-      {SmokeTest.Supervisor, 4010},
-      {PrimeTime.Supervisor, 4020},
-      {MeansToAnEnd.Supervisor, 4030}
+      {SmokeTest.Supervisor, 5010},
+      {PrimeTime.Supervisor, 5020},
+      {MeansToAnEnd.Supervisor, 5030},
+      {BudgetChat.Supervisor, 5040}
     ]
 
     opts = [strategy: :one_for_one, name: ProtoHackers.Supervisor]
